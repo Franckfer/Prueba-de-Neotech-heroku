f5718b741a74f599e4e886aef97fc8ebd3091034
Franckfer <francodel4to@gmail.com>
2021-11-11 19:27:44 -0300

    subiendo repo a git

---
+++ NeoTech/src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "urzobmmmu35dcyj6",
+    "password": "4cbO9OQWl3DMsoVX8l3m",
+    "database": "bc5hismvefes2qnuiult",
+    "host": "bc5hismvefes2qnuiult-mysql.services.clever-cloud.com",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
